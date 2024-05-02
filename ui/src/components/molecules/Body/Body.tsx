c1d84bc32ece8c0bca40ddcc088fb7e50cdb6236
Malek Lahbib <111009238+MalekLahbib@users.noreply.github.com>
2024-05-02 19:59:28 +0200

    02-05

--- ui/src/components/molecules/Body/Body.tsx
+++ ui/src/components/molecules/Body/Body.tsx
@@ -10,6 +10,18 @@ const Body = () => {
   const { provider } = useContext(ProviderContext);
   const [displayedCampaigns, setDisplayedCampaigns] = useState<ICampaign[]>([]);
   const [showForm, setShowForm] = useState<boolean>(false);
+  const [beneficiary, setBeneficiary] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [goal, setGoal] = useState<number>(0);
+  const [begin, setBegin] = useState<number>(0);
+  const [deadline, setDeadline] = useState<number>(0);
+
+  const addDays = (date: Date, days: number) => {
+    const copy = new Date(date.getTime());
+    copy.setDate(copy.getDate() + days);
+    return copy.getTime();
+  };
 
   const fetchCampaigns = async ( ): Promise<ICampaign[]> => {
     if (!provider) {
@@ -28,37 +40,37 @@ const Body = () => {
     return (
         <>
             {showForm ? (
-                <div className="bg-white dark:bg-gray-800 rounded-lg p-4 shadow-md z-10 w-1/2">
+                <div className="bg-white dark:bg-gray-800 rounded-lg p-4 shadow-md z-10 w-1/2 border border-gray-200">
                     <div className="flex items-center justify-center">
                         <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Create Campaign</h2>
                     </div>
                     <form className="mt-4">
-                    <label className="flex justify-between items-center text-sm flex-row">
-                            <span className="text-gray-700 dark:text-gray-400">Beneficiary : </span>
-                            <input type="text" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" placeholder='g1ngj4vvjesklx9awy809expyvae9jt765pt3xe6'/>
-                        </label>
-                        <label className="flex justify-between items-center flex-row text-sm">
-                            <span className="text-gray-700 dark:text-gray-400">Title : </span>
-                            <input type="text" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" />
-                        </label>
-                        <label className="flex justify-between items-center flex-row text-sm mt-4">
-                            <span className="text-gray-700 dark:text-gray-400">Description : </span>
-                            <textarea className="form-textarea w-11/12 mt-1 block rounded-md border border-indigo-600" rows={3} />
-                        </label>
-                        <label className="flex justify-between items-center flex-row text-sm mt-4">
-                            <span className="text-gray-700 dark:text-gray-400">Goal : </span>
-                            <input type="text" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" />
-                        </label>
-                        <label className="flex justify-between items-center flex-row text-sm mt-4">
-                            <span className="text-gray-700 dark:text-gray-400">Begin : </span>
-                            <input type="datetime-local" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" />
-                        </label>
-                        <label className="flex justify-between items-center flex-row text-sm mt-4">
-                            <span className="text-gray-700 dark:text-gray-400">Deadline (days after begin) : </span>
-                            <input type="number" className="form-input w-auto mt-1 block rounded-md border border-indigo-600" />
-                        </label>
-                        
-                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleAddCampaign}>Create Campaign</button>
+                      <label className="flex justify-between items-center text-sm flex-row">
+                        <span className="text-gray-700 dark:text-gray-400">Beneficiary : </span>
+                        <input type="text" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" placeholder='g1ngj4vvjesklx9awy809expyvae9jt765pt3xe6' onBlur={e => setBeneficiary(e.target.value)} />
+                      </label>
+                      <label className="flex justify-between items-center flex-row text-sm">
+                        <span className="text-gray-700 dark:text-gray-400">Title : </span>
+                        <input type="text" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" onBlur={e => setTitle(e.target.value)}/>
+                      </label>
+                      <label className="flex justify-between items-center flex-row text-sm mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">Description : </span>
+                        <textarea className="form-textarea w-11/12 mt-1 block rounded-md border border-indigo-600" rows={3} onBlur={e => setDescription(e.target.value)}/>
+                      </label>
+                      <label className="flex justify-between items-center flex-row text-sm mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">Goal : </span>
+                        <input type="number" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" placeholder='GNOT' onBlur={e => setGoal(Number(e.target.value))}/>
+                      </label>
+                      <label className="flex justify-between items-center flex-row text-sm mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">Begin : </span>
+                        <input type="datetime-local" className="form-input w-5/6 mt-1 block rounded-md border border-indigo-600" onBlur={(e) => setBegin(Date.parse(e.target.value))} />
+                      </label>
+                      <label className="flex justify-between items-center flex-row text-sm mt-4">
+                        <span className="text-gray-700 dark:text-gray-400">Deadline (days after begin) : </span>
+                        <input type="number" className="form-input w-auto mt-1 block rounded-md border border-indigo-600" onBlur={e => setDeadline(addDays(new Date(begin), Number(e.target.value)))}/>
+                      </label>
+                      
+                      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={handleAddCampaign}>Create Campaign</button>
                     </form>
                 </div>
             ) : 
@@ -72,7 +84,7 @@ const Body = () => {
     if (!provider) {
       throw new Error('invalid chain RPC URL');
     }
-    console.log("addcampaign");
+    console.log("owner: ", address, "\nbeneficiary: ", beneficiary, "\ntitle: ", title, "\ndescription: ", description, "\ngoal: ", goal, "\nbegin: ", begin, "\ndeadline: ", deadline);
     setShowForm(false);
     // await provider.evaluateExpression(
     //   Config.REALM_PATH,
